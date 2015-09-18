1458945cd8fd0767221e0de8ca16483d3b207f01
Brad Donald Latendresse <blatendr@silk1.uvm.edu>
2015-09-18 11:26:06 -0400

    First commit

---
+++ assignment2.0/footer.php
@@ -0,0 +1,8 @@
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Brad Latendresse</p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+
+</body>
+</html>
---
+++ sitemap.php
@@ -0,0 +1,34 @@
+
+
+        <!DOCTYPE HTML>
+        <html lang="en">
+
+        <head>
+            <title>Site Map</title>
+        <link rel="stylesheet"
+              href="sitemapstyle.css"
+              type="text/css"
+              media="screen">
+        
+        </head>
+
+        <body id = "main" background= 'sitemap.jpg'>
+
+        <p>Assignment 2.0:</p>
+        <ol>
+            <li><a href="assignment2.0/index.php">index.php</a></li>
+            
+        </ol>
+        
+            
+            
+        <p>Assignment 1.0:</p>
+        <ol>
+            <li><a href="assignment1.0/index.php">index.php</a></li>
+            
+        </ol>
+        
+        
+        
+        </body>
+        </html>
